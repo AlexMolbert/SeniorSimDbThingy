547a960b2e9b4479dc52fc452a92fa66f08865fe
AlexMolbert <alexandermolbert@hotmail.com>
2016-09-15 10:26:45 -0500

    Update home.js

--- home/home.js
+++ home/home.js
@@ -1,7 +1,7 @@
 'use strict';
  
 angular.module('myApp.home', ['ngRoute'])
- 
+angular.module('myApp.home', ['ngRoute','firebase'])
 // Declared route 
 .config(['$routeProvider', function($routeProvider) {
     $routeProvider.when('/home', {
@@ -11,6 +11,37 @@ angular.module('myApp.home', ['ngRoute'])
 }])
  
 // Home controller
-.controller('HomeCtrl', [function() {
+.controller('HomeCtrl', ['$scope','$firebaseSimpleLogin',function($scope,$firebaseSimpleLogin) {
+ <script src="https://www.gstatic.com/firebasejs/3.4.0/firebase.js"></script>
+ var loginObj = $firebaseSimpleLogin(firebaseObj);
+ <script>
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyAC5PWxy2MnvqULjTidMw_PSNj1BJdV8bs",
+    authDomain: "seniorinsertheredesign.firebaseapp.com",
+    databaseURL: "https://seniorinsertheredesign.firebaseio.com",
+    storageBucket: "seniorinsertheredesign.appspot.com",
+    messagingSenderId: "30344832894"
+  };
+  firebase.initializeApp(config);
+ </script>
  
+ 
+ $scope.SignIn = function(event) {
+    event.preventDefault();  // To prevent form refresh
+    var username = $scope.user.email;
+    var password = $scope.user.password;
+     
+    loginObj.$login('password', {
+            email: username,
+            password: password
+        })
+        .then(function(user) {
+            // Success callback
+            console.log('Authentication successful');
+        }, function(error) {
+            // Failure callback
+            console.log('Authentication failure');
+        });
+}
 }]);
